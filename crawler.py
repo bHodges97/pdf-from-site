e25b4ce501f96825b92860d2fe3eb798667b1b94
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-09-25 16:27:17 +0100

    added paths

--- crawler.py
+++ crawler.py
@@ -9,11 +9,7 @@ class Crawler():
     Crawls google scholar for pdfs
     Use download_all to download the pdfs
     """
-    def __init__(self):
-        #pretend to be browser to avoid 403 error
-        self.outfile = "out.csv"
-
-    def query(self,query,limit = 50):
+    def query(self,path="out.csv",query,limit = 50):
         search_query = scholarly.search_pubs_query(query)
         count = 0
         pubs = []
@@ -23,21 +19,21 @@ class Crawler():
             f.write(",".join(fieldnames) + "\n")
         for x in search_query:
             if 'eprint' in x.bib:
-                with open(self.outfile,"a") as f:
+                with open(path,"a") as f:
                     print("Found ",x.bib["title"])
                     csv.DictWriter(f, fieldnames=fieldnames).writerow(x.bib)
                 count+=1
                 if count == limit:
                     break
 
-    def download_all(self):
+    def download_all(self,path="out.csv",out="./downloads"):
         files = []
-        with open(self.outfile) as csvfile:
+        with open(path) as csvfile:
             reader = csv.DictReader(csvfile)
             for idx,row in enumerate(reader):
                 print(row['title'])
                 url = row['eprint']
-                out = download(url,save_copy=True)
+                out = download(url,path=out,save_copy=True)
                 files.append((out,row['title']))
 
         return files
